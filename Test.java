13f96abf9cff66a1f055bda81aa558bea3469c5b
sgarrett0 <sgarrett@salesianum.org>
2022-03-31 09:37:37 -0400

    my test  branch

--- Test.java
+++ Test.java
@@ -4,5 +4,7 @@ public class Test{
         System.out.println("testing git merges conflicts");
         System.out.println("change 1");
         //change 1
+        System.out.println("TESTING BRANCHES");
+        System.out.print("making change sto code to reflect the use of branches.");
     }
 }
